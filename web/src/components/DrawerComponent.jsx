29fea339b0d59cc78321c623f0405fadc6733ae3
Filipe Batista <filipe.batista@externos-br.cencosud.com>
2022-12-16 17:45:41 -0300

    removendo a possibilidade de ver o devtools

--- web/src/components/DrawerComponent.jsx
+++ web/src/components/DrawerComponent.jsx
@@ -152,6 +152,40 @@ function DrawerComponent({ title, children }) {
     }
   }, [])
 
+  React.useEffect(() => {
+    const handleContextmenu = e => {
+      e.preventDefault()
+    }
+    document.addEventListener('contextmenu', handleContextmenu)
+
+    document.onkeydown = function (e) {
+
+      // disable F12 key
+      if (e.keyCode == 123) {
+        return false;
+      }
+
+      // disable I key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+        return false;
+      }
+
+      // disable J key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        return false;
+      }
+
+      // disable U key
+      if (e.ctrlKey && e.keyCode == 85) {
+        return false;
+      }
+    }
+    
+    return function cleanup() {
+      document.removeEventListener('contextmenu', handleContextmenu)
+    }
+  }, [])
+
   return (
     <ThemeProvider theme={mode === 'light' ? mdTheme : mdThemeDark}>
       <Box sx={{ display: 'flex' }}>
